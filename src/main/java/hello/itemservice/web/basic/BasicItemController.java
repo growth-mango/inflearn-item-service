c45ab2483e0b2514c2237ce1ddd0960cbc5be7d7
mango <esudatcom@gmail.com>
2024-01-08 19:10:24 +0900

    상품 등록 처리 PostMapping 추가

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,6 +35,35 @@ public class BasicItemController {
     @GetMapping("/add")
     public String addForm() {
         return "basic/addForm";
+    }
+
+    // @PostMapping("/add")
+    public String addItemV1(@RequestParam(name="itemName") String itemName,
+                       @RequestParam(name="price") int price,
+                       @RequestParam(name="quantity") Integer quantity,
+                       Model model){
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    // ModelAttribute : Model 객체 만들어주기 , Model에 넣어주는 두 가지 역할을 모두 수행한다.
+    public String addItemV2(@ModelAttribute("item") Item item) {
+
+        itemRepository.save(item);
+
+//        model.addAttribute("item", item); // 자동 추가 되기 때문에 생략 가능하다.
+
+        return "basic/item";
     }
 
     // 테스트용 데이터 추가
